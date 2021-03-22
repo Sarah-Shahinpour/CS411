5e53983886348adc425071980905419505e00c0a
JasonChow320 <79327473+JasonChow320@users.noreply.github.com>
2021-03-22 12:06:40 -0400

    Create route.js

---
+++ server/routes/route.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/users');
+
+// retrieve user data
+router.get('/users', (req, res, next) => {
+    User.find(function(err, users){
+        res.json(users);
+    })
+});
+
+// add user data
+router.post('/users', (req, res, next)=>{
+    let newUser = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save((err, user)=>{
+        if(err){
+            res.json({msg: 'Failed to add user'});
+        }else{
+            res.json({msg: 'User added successfully'});
+        }
+    });
+});
+
+//delete contacts
+router.delete('/users/:id', (req, res, next)=> {
+    User.deleteOne({_id: req.params.id}, function(err, result){
+        if(err){
+            res.json(err);
+        } else{
+            res.json(result);
+        }
+    });
+});
+
+module.exports = router;
