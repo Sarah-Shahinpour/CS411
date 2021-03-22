f8f1d3b187d542be9597c1a3bf865fd45e41b8da
JasonChow320 <79327473+JasonChow320@users.noreply.github.com>
2021-03-22 12:07:56 -0400

    Update route.js

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require('../models/users');
+const User = require('../models/user');
 
 // retrieve user data
 router.get('/users', (req, res, next) => {
