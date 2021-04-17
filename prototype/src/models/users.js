0a81e8d36987d54f26eabc3c501712d4f2c0f92f
Jason Chow <79327473+JasonChow320@users.noreply.github.com>
2021-04-17 19:38:10 -0400

    Update users.js
    Now user schema is used to store data, while login schema is used to authenticate users

--- prototype/src/models/users.js
+++ prototype/src/models/users.js
@@ -1,14 +1,30 @@
 const mongoose = require('mongoose');
 
 const UserSchema = mongoose.Schema({
-    username:{
-        type: String,
-        required: true
-    },
-    password:{
-        type: String,
-        required: true
+    spotify:{
+        havespotify:{
+            type: Boolean,
+            required: true
+        },
+        spotifytoken:{
+            type: String,
+            required: false
+        }
     }
+});
+
+//check if user have a spotify account login
+UserSchema.virtual('havespotify')
+.get(function(){
+    return this.spotify.havespotify;
+})
+.set(function(b){
+    this.spotify.havespotify = b;
+});
+
+//check what the username is
+UserSchema.virtual('refreshtoken').set(function(str){
+    this.spotify.spotifytoken = str;
 });
 
 const User = module.exports = mongoose.model('User', UserSchema);
