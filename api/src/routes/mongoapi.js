43100a310ea2b336160d97ae86891a0b2a4f7123
Jason Chow <79327473+JasonChow320@users.noreply.github.com>
2021-04-21 18:36:42 -0400

    Update and rename prototype/src/routes/mongoapi.js to api/src/routes/mongoapi.js

--- api/src/routes/mongoapi.js
+++ api/src/routes/mongoapi.js
@@ -1,25 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
+const config = require('../config/config');
 const User = require('../models/users');
 
 // retrieve user data
-router.get('/users', (req, res, next) => {
-    User.find(function(err, users){
-        res.status(200).json(users);
-    })
+router.get('/users/:id', (req, res, next) => {
+    var _id = req.params.id;
+    if(_id == config.crypto.admin){
+        User.find(function(err, users){
+            res.status(200).json(users);
+        });
+    }else{
+        console.log('ID ' + _id + ' TRIED TO REQUEST USER DATA!!!');
+        next(); //gives back 404 response
+    }
 });
 
 // add user data
-router.post('/users', (req, res, next)=>{
+router.post('/user', (req, res, next)=>{
     let newUser = new User({
         username: req.body.username,
-        password: req.body.password
+        password: req.body.password,
+        spotify:{
+            havespotify: false,
+        }
     });
 
     newUser.save((err, user)=>{
         if(err){
-            res.status(200).json({msg: 'Failed to add user'});
+            res.status(200).json({msg: 'Failed to add user', error : err});
         }else{
             res.status(200).json({msg: 'User added successfully'});
         }
