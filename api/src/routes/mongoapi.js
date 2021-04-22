e1fbf0900112c0deeb23d201d4a9a27aa39a3d44
Jason Chow <79327473+JasonChow320@users.noreply.github.com>
2021-04-21 23:40:31 -0400

    Update mongoapi.js

--- api/src/routes/mongoapi.js
+++ api/src/routes/mongoapi.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const config = require('../config/config');
-const User = require('../models/users');
+const User = require('../models/user');
 
 // retrieve user data
 router.get('/users/:id', (req, res, next) => {
